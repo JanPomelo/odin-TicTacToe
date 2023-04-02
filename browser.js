5daa850313c12fa93fba6b8b12f5b03a4ed03354
Jan Steikowski <j.steikowski@aol.com>
2023-04-02 21:50:53 +0700

    Add mediaqueries for the intro and add persons to js

--- browser.js
+++ browser.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Gameboard = (() => {
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
   const getgameBoard = () => {
@@ -9,6 +11,18 @@ const Gameboard = (() => {
   return {getgameBoard, setMarkOnGameBoard};
 })();
 
+const Player = (name, mark) => {
+  const sayHello = () => {
+    return `Hello, my name is ${name} and my mark is ${mark}.`;
+  };
+  return {sayHello};
+};
+
+const player1 = Player('Jan', 'X');
+const player2 = Player('Computer', 'O');
+
 console.log(Gameboard.getgameBoard());
 Gameboard.setMarkOnGameBoard('X', 2);
 console.log(Gameboard.getgameBoard());
+console.log(player1.sayHello());
+console.log(player2.sayHello());
