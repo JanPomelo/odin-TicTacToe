3d2a0a8fdf9d9d167d2608d78073aaf47cd2a515
Jan Steikowski <j.steikowski@aol.com>
2023-04-07 16:40:17 +0700

    Add winning conditions

--- browser.js
+++ browser.js
@@ -53,13 +53,79 @@ const GameController = (() => {
     return Math.floor(Math.random() * max);
   };
 
+  const checkVertical = (board, mark, col) => {
+    if (board[1][col] === mark) {
+      if (board[2][col] === mark) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkHorizontal = (board, mark, row) => {
+    if (board[row][1] === mark) {
+      if (board[row][2] === mark) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkDiagonal = (board, mark) => {
+    if (board[0][0] === mark) {
+      if (board[1][1] === mark) {
+        if (board[2][2] === mark) {
+          return true;
+        }
+      }
+    }
+    if (board[0][2] === mark) {
+      if (board[1][1] === mark) {
+        if (board[2][0] === mark) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const checkWinner = (mark) => {
+    const board = Gameboard.getGameBoard();
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][0] === mark) {
+        if (i === 0) {
+          for (let j = 0; j < board[i].length; j++) {
+            if (checkVertical(board, mark, j)) {
+              return true;
+            }
+          }
+        }
+        if (checkHorizontal(board, mark, i)) {
+          console.log('horizontal');
+          return true;
+        }
+      }
+    }
+    if (checkDiagonal(board, mark)) {
+      return true;
+    }
+    return false;
+  };
+
   const playRound = (row, col) => {
     let pcMark = false;
     if (players[0].setMark(row, col) === true) {
       Gameboard.printGameBoard();
+      if (checkWinner(players[0].mark) === true) {
+        // endGame(players[0]);
+        console.log('Congrats, you won the game!');
+      }
       do {
         pcMark = players[1].setMark(rndmNumber(3), rndmNumber(3));
       } while (!pcMark);
+      if (checkWinner(players[1].mark)) {
+        console.log('The ugly thief won the game!');
+      }
     }
   };
 
